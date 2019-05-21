c9a02e3f06c87f4e2fb38b7a931c756d566f65bb
josireispereira <josireispereira@gmail.com>
2019-05-21 17:10:57 -0300

    adiciona js da page duvidas

---
+++ duvidas/js/script.js
@@ -0,0 +1,11 @@
+const plus = document.getElementsByClassName('btn-expand');
+const minus = document.getElementsByClassName('btn-hide');
+
+function a (); 
+verificar se btn tem classe active ativada
+toggle
+elemento.classList.toggle('classe')
+duas linhas
+
+plus.addEventListener("click", function a(){});
+minus.addEventListener("click", function a(){});
